3c4f9df23c9f5c8ff3855b0f3eaae0bdd5d8ccfe
smurfjames101 <smurfjames@gmail.com>
2019-02-15 15:12:23 +0000

    redeployed

--- src/main/java/com/qa/rest/ClassroomEndpoint.java
+++ src/main/java/com/qa/rest/ClassroomEndpoint.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -50,7 +51,7 @@ public class ClassroomEndpoint {
 	}
 	
 	@Path("/updateClassroom/{classroom}/{id}")
-	@GET
+	@PUT
 	@Produces({ "application/json" })
 	public String updateClassroom(@PathParam("classroom") String classroom, @PathParam("id") Long id) {
 		return service.updateClassroom(classroom, id);
--- src/main/java/com/qa/rest/TraineeEndpoint.java
+++ src/main/java/com/qa/rest/TraineeEndpoint.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,6 +46,12 @@ public class TraineeEndpoint {
 	public String deleteTrainee(@PathParam("id") Long id)
 	{
 		return service.deleteTrainee(id);
+	}
+	@Path("/updateTrainee/{trainee}/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateTrainee(@PathParam("trainee") String trainee, @PathParam("id") Long id) {
+		return service.updateTrainee(trainee, id);
 	}
 	
 	public void setService(TraineeService service) {
